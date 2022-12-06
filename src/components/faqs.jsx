26c5c522015e198adf1361b8b6a350e7aa65705a
Jshin720 <jeffshin720@gmail.com>
2022-12-06 18:02:27 -0500

    making the skeleton frame for the a card

---
+++ src/components/faqs.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function faqs() {
+
+  return (
+    <div>
+      <h1 className="aheader">FAQS</h1>
+      <body>
+        <span>
+          <h4></h4>
+          <p></p>
+        </span>
+        <span>
+          <h4></h4>
+          <p></p>
+        </span>
+        <span>
+          <h4></h4>
+          <p></p>
+        </span>
+        <span>
+          <h4></h4>
+          <p></p>
+        </span>
+        <span>
+          <h4></h4>
+          <p></p>
+        </span>
+      </body>
+    </div>
+  );
+}
+
+export default faqs;
